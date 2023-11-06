3eacd4bfe430a6b09cccca2fc40dd15bc60bab41
Lukas Valentino <91643553+lukasvalentino971@users.noreply.github.com>
2023-11-06 20:52:45 +0700

    fix: change column to stack

--- chat_ui/lib/main.dart
+++ chat_ui/lib/main.dart
@@ -24,89 +24,93 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFF171717),
-      body: Column(
+      body: Stack(
         children: [
-          Padding(
-            padding: const EdgeInsets.only(top: 70, left: 5, right: 5),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                IconButton(
-                    onPressed: () {},
-                    icon: const Icon(
-                      Icons.menu,
-                      color: Colors.white,
-                    )),
-                IconButton(
-                  onPressed: () {},
-                  icon: const Icon(
-                    Icons.search,
-                    color: Colors.white,
-                  ),
-                ),
-              ],
-            ),
-          ),
-          SizedBox(
-            height: 50,
-            child: ListView(
-              scrollDirection: Axis.horizontal,
-              padding: const EdgeInsets.only(left: 10),
-              children: [
-                TextButton(
-                  onPressed: () {},
-                  child: const Text(
-                    "Messages",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 25,
+          Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 70, left: 5, right: 5),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.menu,
+                          color: Colors.white,
+                        )),
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.search,
+                        color: Colors.white,
+                      ),
                     ),
-                  ),
+                  ],
                 ),
-                const SizedBox(
-                  width: 35,
-                ),
-                TextButton(
-                  onPressed: () {},
-                  child: const Text(
-                    "Online",
-                    style: TextStyle(
-                      color: Colors.grey,
-                      fontSize: 25,
+              ),
+              SizedBox(
+                height: 50,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  padding: const EdgeInsets.only(left: 10),
+                  children: [
+                    TextButton(
+                      onPressed: () {},
+                      child: const Text(
+                        "Messages",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 25,
+                        ),
+                      ),
                     ),
-                  ),
-                ),
-                const SizedBox(
-                  width: 35,
-                ),
-                TextButton(
-                  onPressed: () {},
-                  child: const Text(
-                    "Group",
-                    style: TextStyle(
-                      color: Colors.grey,
-                      fontSize: 25,
+                    const SizedBox(
+                      width: 35,
                     ),
-                  ),
-                ),
-                const SizedBox(
-                  width: 35,
-                ),
-                TextButton(
-                  onPressed: () {},
-                  child: const Text(
-                    "More",
-                    style: TextStyle(
-                      color: Colors.grey,
-                      fontSize: 25,
+                    TextButton(
+                      onPressed: () {},
+                      child: const Text(
+                        "Online",
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 25,
+                        ),
+                      ),
                     ),
-                  ),
+                    const SizedBox(
+                      width: 35,
+                    ),
+                    TextButton(
+                      onPressed: () {},
+                      child: const Text(
+                        "Group",
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 25,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 35,
+                    ),
+                    TextButton(
+                      onPressed: () {},
+                      child: const Text(
+                        "More",
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 25,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 35,
+                    ),
+                  ],
                 ),
-                const SizedBox(
-                  width: 35,
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
         ],
       ),
