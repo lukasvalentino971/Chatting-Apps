f876f4fa5c5841b915602a9f8c9efb084cbfd23e
Lukas Valentino <91643553+lukasvalentino971@users.noreply.github.com>
2023-11-06 20:45:55 +0700

    feat: navbar

---
+++ chat_ui/lib/main.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'New Whatsapp UI',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF171717),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 70, left: 5, right: 5),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.menu,
+                      color: Colors.white,
+                    )),
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.search,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 50,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              padding: const EdgeInsets.only(left: 10),
+              children: [
+                TextButton(
+                  onPressed: () {},
+                  child: const Text(
+                    "Messages",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 25,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  width: 35,
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: const Text(
+                    "Online",
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 25,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  width: 35,
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: const Text(
+                    "Group",
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 25,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  width: 35,
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: const Text(
+                    "More",
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 25,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  width: 35,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
